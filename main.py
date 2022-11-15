1d7f395ba6f98fbbf5665578f55be2b2e763dc01
M216916 <86331863+M216916@users.noreply.github.com>
2022-11-15 17:30:10 +0900

    comment delete

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import torch
@@ -10,14 +9,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_lightning import Trainer
 
-
 from util.env import get_device, set_device
 from util.preprocess import build_loc_net, construct_data
 from util.net_struct import get_feature_map, get_fc_graph_struc
 from util.iostream import printsep
 
 from datasets.TimeDataset import TimeDataset
-
 
 from models.GDN import GDN
 
@@ -41,102 +38,63 @@ warnings.filterwarnings('ignore')
 class Main():
     def __init__(self, train_config, env_config, debug=False):
 
-        self.train_config = train_config        # batch:32, epoch:3, slide_win:5, dim:64, slide_stride:1, comment:msl, seed:5
-                                                # out_layer_num:1, out_layer_inter_dim:128, decay: 0.0, val_ratio:0.2, topk:5
-        self.env_config = env_config            # save_path:msl, dataset:msl, report:best, device:cpu, load_model_path: ''
+        self.train_config = train_config
+        self.env_config = env_config
         self.datestr = None
 
-        dataset = self.env_config['dataset']                                              # msl
-        train_orig = pd.read_csv(f'./data/{dataset}/train.csv', sep=',', index_col=0)     # train : (1565,27)
-        test_orig = pd.read_csv(f'./data/{dataset}/test.csv', sep=',', index_col=0)       # test  : (2049,28) columnに'attack'がある
+        dataset = self.env_config['dataset']
+        train_orig = pd.read_csv(f'./data/{dataset}/train.csv', sep=',', index_col=0)
+        test_orig = pd.read_csv(f'./data/{dataset}/test.csv', sep=',', index_col=0)
        
         train, test = train_orig, test_orig
 
         if 'attack' in train.columns:
             train = train.drop(columns=['attack'])
 
-        feature_map = get_feature_map(dataset)                                            # M-6, M-1, M-2, S-2 … : len 27
-        fc_struc = get_fc_graph_struc(dataset)                                            # M-6:[M-1, M-2, S-2 …], M-1:[M-6, M-2, S-2 …],  … : ノード全702組
+        feature_map = get_feature_map(dataset)
+        fc_struc = get_fc_graph_struc(dataset)
 
         set_device(env_config['device'])
-        self.device = get_device()                                                        # cpu
+        self.device = get_device()
 
         fc_edge_index = build_loc_net(fc_struc, list(train.columns), feature_map=feature_map)
         fc_edge_index = torch.tensor(fc_edge_index, dtype = torch.long)
-                  # tensor[[ 1, 2, 3, ... , 24, 25, 26, 0, 2 ,3, ... , 24, 25, 26, ... ,  0,  1,  2  ... , 23, 24, 25],
-                  #        [ 0, 0, 0, ... ,  0,  0,  0, 1, 1, 1, ... ,   1, 1,  1, ... , 26, 26, 26, ... , 26, 26, 26]] → (2,702) : len 2   torch.int64に変換
         self.feature_map = feature_map
 
-        train_dataset_indata = construct_data(train, feature_map, labels=0)                   # (28, 1565)  # train には28行目に [0, 0, ... , 0] を追加
-        test_dataset_indata = construct_data(test, feature_map, labels=test.attack.tolist())  # (28, 2049)  # test  には28行目に元の attack 列
+        train_dataset_indata = construct_data(train, feature_map, labels=0)
+        test_dataset_indata = construct_data(test, feature_map, labels=test.attack.tolist())
 
-
-        cfg = {
-            'slide_win': train_config['slide_win'],                                           # slide_win    : 5
-            'slide_stride': train_config['slide_stride'],                                     # slide_stride : 1
-        }
+        cfg = {'slide_win': train_config['slide_win'], 
+               'slide_stride': train_config['slide_stride'],}
 
         train_dataset = TimeDataset(train_dataset_indata, fc_edge_index, mode='train', config=cfg)    
         test_dataset = TimeDataset(test_dataset_indata, fc_edge_index, mode='test', config=cfg)
-        
-             #【train】                                          #【test】
-             #    [0][0]     [0][1]    [0][2]    [0][3]          #    [0][0]     [0][1]    [0][2]    [0][3]
-             #    (27,5)      (27)      (0)      (2,702)         #    (27,5)      (27)    (0 or 1)   (2,702)
-             #       …         …         …         …             #       …         …         …         …
-             #       …         …         …         …             #       …         …         …         …
-             # [1559][0]  [1559][1]  [1559][2]  [1559][3]        # [2043][0]  [2043][1]  [2043][2]  [2043][3]
-             #    (27,5)      (27)      (0)      (2,702)         #    (27,5)      (27)    (0 or 1)   (2,702)
 
         train_dataloader, val_dataloader = self.get_loaders(train_dataset, train_config['seed'], train_config['batch'], val_ratio = train_config['val_ratio'])
 
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
 
-
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
         self.test_dataloader = DataLoader(test_dataset, batch_size=train_config['batch'],
                             shuffle=False, num_workers=0)
 
-                                        # import pprint
-                                        # pprint.pprint(vars(self.test_dataloader))
-        
-                                        # 【self.train_dataloader】【self.val_dataloader】【self.test_dataloader】
-                                        # _DataLoader__initialized             : True
-                                        # _DataLoader__multiprocessing_context : None
-                                        # _IterableDataset_len_called          : None
-                                        # _dataset_kind                        : 0
-                                        # batch_sampler                        : <torch.utils.data.sampler.BatchSampler object at ~>
-                                        # batch_size                           : 32
-                                        # collate_fn                           : <function default_collate at ~>
-                                        # dataset                              : <torch.utils.data.dataset.Subset object at ~>
-                                        # drop_last                            : False
-                                        # num_workers                          : 0
-                                        # pin_memory                           : False
-                                        # sampler                              : <torch.utils.data.sampler.RandomSampler object at ~>
-                                        # timeout                              : 0
-                                        # worker_init_fn                       : None
-
-        
-        
-
         edge_index_sets = []
-        edge_index_sets.append(fc_edge_index)                               # edge_index_sets[0][0] : 1,2,3,...,25,26,0,2,3,...,25,26,..., 0, 1, 2,...,24,25 (702)
-                                                                            #                [0][1] : 0,0,0,..., 0, 0,1,1,1,..., 1, 1,...,26,26,26,...,26,26 (702)
+        edge_index_sets.append(fc_edge_index)
 
-        self.model = GDN(edge_index_sets, len(feature_map),                 # len(feature_map) = 27
-                dim=train_config['dim'],                                    # 64
-                input_dim=train_config['slide_win'],                        # 5
-                out_layer_num=train_config['out_layer_num'],                # 1
-                out_layer_inter_dim=train_config['out_layer_inter_dim'],    # 128
-                topk=train_config['topk']                                   # 5
-            ).to(self.device)                                               # cpu
+        self.model = GDN(edge_index_sets, len(feature_map),
+                dim=train_config['dim'],
+                input_dim=train_config['slide_win'],
+                out_layer_num=train_config['out_layer_num'],
+                out_layer_inter_dim=train_config['out_layer_inter_dim'],
+                topk=train_config['topk'] ).to(self.device) 
 
 
     def run(self):
 
-        if len(self.env_config['load_model_path']) > 0:                     # × (len(self.env_config['load_model_path'] = 0)
-            model_save_path = self.env_config['load_model_path']            # ×
+        if len(self.env_config['load_model_path']) > 0:
+            model_save_path = self.env_config['load_model_path']
 
         else:
             model_save_path = self.get_save_path()[0]                       # ./pretrained/msl/best_09|21-17:31:58.pt
@@ -149,19 +107,15 @@ class Main():
                 test_dataloader=self.test_dataloader,                       # torch.utils.data.dataloader.DataLoader object
                 test_dataset=self.test_dataset,                             # datasets.TimeDataset.TimeDataset object
                 train_dataset=self.train_dataset,                           # datasets.TimeDataset.TimeDataset object
-                dataset_name=self.env_config['dataset']                     # msl
-            )                                                               # self.train_log : len(39 * epoch)
+                dataset_name=self.env_config['dataset'] )
         
         # test            
-        self.model.load_state_dict(torch.load(model_save_path))             # ./pretrained/make/best_****.pt から最新モデルを読み込み
-        best_model = self.model.to(self.device)                             # best_model
+        self.model.load_state_dict(torch.load(model_save_path))
+        best_model = self.model.to(self.device)
 
-        _, self.test_result, conv_list = test(best_model, self.test_dataloader)        # _:スカラー ／ self.test_result:(3,2044,27)
-        _, self.val_result, __ = test(best_model, self.val_dataloader)          # _:スカラー ／ self.test_result:(3, 312,27)
+        _, self.test_result, conv_list = test(best_model, self.test_dataloader)
+        _, self.val_result, __ = test(best_model, self.val_dataloader)
 
-
-
-        #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
         dataset = self.env_config['dataset']
         dim = self.train_config['dim']
         t = pd.read_csv(f'./data/{dataset}/true.csv')
@@ -180,21 +134,18 @@ class Main():
 
         self.LightGBM(data)
 
-        #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-        self.get_score(self.test_result, self.val_result)                   # None
+        self.get_score(self.test_result, self.val_result)
+
 
     def get_loaders(self, train_dataset, seed, batch, val_ratio=0.1):
         dataset_len = int(len(train_dataset))                               # 1560
         train_use_len = int(dataset_len * (1 - val_ratio))                  # 1248
         val_use_len = int(dataset_len * val_ratio)                          # 312
         val_start_index = random.randrange(train_use_len)                   # 523
-        indices = torch.arange(dataset_len)                                 # tensor[   0,    1,    2,  ..., 1557, 1558, 1559]
+        indices = torch.arange(dataset_len)                                 
 
         train_sub_indices = torch.cat([indices[:val_start_index], indices[val_start_index+val_use_len:]])
-                                                                            # indices[:val_start_index] : tensor[   0,    1,    2,  ...,  520,  521,  522]
-                                                                            # indices[ + val_use_len:]  : tensor[ 835,  836,  837,  ..., 1557, 1558, 1559]
-        train_subset = Subset(train_dataset, train_sub_indices)             # train_sub_indices         : len 1248
-                                                                            # torch.utils.data.dataset.Subset object
+        train_subset = Subset(train_dataset, train_sub_indices)
         val_sub_indices = indices[val_start_index:val_start_index+val_use_len]
         val_subset = Subset(train_dataset, val_sub_indices)
 
@@ -204,11 +155,11 @@ class Main():
         return train_dataloader, val_dataloader
     
 
-    def get_score(self, test_result, val_result):                                                  # test_result[2][2043] : len(27) ／ val_result[2][311] : len(27)
+    def get_score(self, test_result, val_result):
 
-        feature_num = len(test_result[0][0])                                                       # 27 (すべて 0.0 or 1.0)
-        np_test_result = np.array(test_result)                                                     # (3, 2044, 27)
-        np_val_result = np.array(val_result)                                                       # (3,  312, 27)
+        feature_num = len(test_result[0][0])
+        np_test_result = np.array(test_result)
+        np_val_result = np.array(val_result)
  
         GDN_num = os.getcwd().replace('/home/inaba/', '')
         dataset = self.env_config['dataset']
@@ -232,16 +183,16 @@ class Main():
 
     def get_save_path(self, feature_name=''):
 
-        dir_path = self.env_config['save_path']                  # msl
+        dir_path = self.env_config['save_path']
         
         if self.datestr is None:
-            now = datetime.now()                                 # 現在の時刻 YYYY-MM-DD HH:MM:SS
-            self.datestr = now.strftime('%m|%d-%H:%M:%S')        # 現在の時刻      MM|DD-HH:MM:SS
+            now = datetime.now()
+            self.datestr = now.strftime('%m|%d-%H:%M:%S')
         datestr = self.datestr          
 
         paths = [                                        
-            f'./pretrained/{dir_path}/best_{datestr}.pt',        # path作成  ./pretrained/msl/best_09|22-16:30:34.pt
-            f'./results/{dir_path}/{datestr}.csv',               # path作成  ./results   /msl/     09|22-16:30:34.csv
+            f'./pretrained/{dir_path}/best_{datestr}.pt',
+            f'./results/{dir_path}/{datestr}.csv',
         ]
 
         for path in paths:
